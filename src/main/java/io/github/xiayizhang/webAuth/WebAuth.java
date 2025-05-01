55b910c39d927b4c06f9e09b0f4b608c3d95c9bb
XiayiZhang <122589407+XiayiZhang@users.noreply.github.com>
2025-05-01 20:42:05 +0800

    Create WebAuth.java

---
+++ src/main/java/io/github/xiayizhang/webAuth/WebAuth.java
@@ -0,0 +1,22 @@
+package io.github.xiyayizhang.webAuth;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+public final class WebAuth extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new PlayerAuthListener(this), this);
+        getLogger().info("WebAuth已启用！");
+    }
+}
